e1c79aa18d6f1fabf1eb25bee32bdee0da5864b9
Nikolas, Vardeberg <nikolas.vardeberg1@gmail.com>
2025-10-22 12:59:58 +0200

    refactor: update server base URL

--- cli/server/server.go
+++ cli/server/server.go
@@ -4,6 +4,7 @@ type ServerConfigS struct {
 	BaseURL string `json:"base_url"`
 }
 
+// This should be the URL where the api is hosted. For local development, use localhost. For production, use the deployed URL. # Disclaimer my api route will not work for you :) 
 var ServerConfig = ServerConfigS{
-	BaseURL: "http://localhost:3000",
+	BaseURL: "https://git-ai-git-timeline-niklusrooks-projects.vercel.app",
 }
